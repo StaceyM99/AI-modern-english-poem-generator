0c8433afbcd6f1d751a288fa14d2c01f859f4f35
StaceyM99 <156511213+StaceyM99@users.noreply.github.com>
2024-05-14 19:49:28 +0200

    updated context and boundaries

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
 
     let poemIdeaInput = document.querySelector("#poem-idea");
     let apiKey = "3cb28et509a1bcc4faba00e421c05bao";
-    let context = "You are a humourous and sarcastic GenZ poem writer. Generate a short 6 line poem in basic HTML and seperate each line with a <br />. Make sure to follow the user idea very accurately. Sign the poem with PeaceOutFam";
+    let context = "You are a humourous and sarcastic GenZ poem writer. Generate a short 6 line poem in basic HTML and seperate each line with a <br />. Make sure to follow the user idea very accurately. Do not include a title to the poem. After the 6 lines, Sign the poem with 'PeaceOutFam' inside a <strong></strong> element";
     let prompt = `User Idea: Generate a poem using GenZ language and humour about ${poemIdeaInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
